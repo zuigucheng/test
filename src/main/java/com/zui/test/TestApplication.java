98a5ae44763738085b3dc4b65ba8b0fdc7b2fba8
zui <2077976082@qq.com>
2019-11-12 16:14:33 +0800

    first commit

---
+++ src/main/java/com/zui/test/TestApplication.java
@@ -0,0 +1,16 @@
+package com.zui.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zui
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
